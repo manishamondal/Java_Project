5982e015a6811efe409d390b9e1ef23fb4735e24
Your Name <you@example.com>
2026-02-27 23:00:22 +0530

    test

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,12 +1,26 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
 
     @Test
-    public void testDummy() {
-        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+    void main_printsHelloWorld() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(buffer));
+
+        try {
+            App.main(new String[0]);
+        } finally {
+            System.setOut(originalOut);
+        }
+
+        assertEquals("Hello, World!\n", buffer.toString());
     }
 }
