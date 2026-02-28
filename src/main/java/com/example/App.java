9ca1eac3a6732a21b95c7543e5952d996fdd85ba
Your Name <you@example.com>
2026-02-28 11:14:47 +0530

    a

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,11 @@
 package com.example;
+
+import java.util.logging.Logger;
+
 public class App {
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        LOGGER.info("Hello, World!");
     }
 }
